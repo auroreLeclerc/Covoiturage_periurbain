73decb6450a76c79788c44a5933d333750de2f30
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2024-01-29 21:04:55 +0100

    stops mac departure arrival

--- server/init.sql
+++ server/init.sql
@@ -4,9 +4,16 @@ DROP DATABASE IF EXISTS cvp;
 CREATE DATABASE cvp;
 USE "cvp";
 
+CREATE TABLE towns (
+	`name` VARCHAR(100) NOT NULL PRIMARY KEY,
+	`code` int NOT NULL
+);
+
 CREATE TABLE stops (
-	`town` VARCHAR(100) NOT NULL PRIMARY KEY,
-	`code` int DEFAULT NULL
+	`id` int PRIMARY KEY,
+	`departure` VARCHAR(100) REFERENCES towns(`name`),
+	`arrival` VARCHAR(100) REFERENCES towns(`name`),
+	`mac` VARCHAR(17) NOT NULL
 );
 
 CREATE TABLE profile (
@@ -14,7 +21,7 @@ CREATE TABLE profile (
 	`name` text DEFAULT NULL,
 	`mail` VARCHAR(320) NOT NULL PRIMARY KEY,
 	`password` text NOT NULL,
-	`town` VARCHAR(100) DEFAULT NULL REFERENCES stops(town),
+	`town` VARCHAR(100) REFERENCES towns(`name`),
 	`phone` text DEFAULT NULL,
 	`registration` datetime DEFAULT CURRENT_TIMESTAMP,
 	`banned` boolean DEFAULT false
@@ -22,16 +29,16 @@ CREATE TABLE profile (
 
 CREATE TABLE driver (
 	`mail` VARCHAR(320) REFERENCES profile(mail) ON DELETE CASCADE,
-	`passengers` int DEFAULT 0,
-	`numberplate` text NOT NULL DEFAULT "XX-000-XX",
-	`mac` text NOT NULL DEFAULT "00:00:00:00:00:00"
+	`passengers` int NOT NULL,
+	`numberplate` text NOT NULL,
+	`mac` VARCHAR(17) NOT NULL
 );
 
 CREATE TABLE travel (
-	`id` int NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+	`id` int PRIMARY KEY AUTO_INCREMENT,
 	`driver` VARCHAR(320) REFERENCES driver(mail) ON DELETE CASCADE,
-	`departure` VARCHAR(100) DEFAULT NULL REFERENCES stops(town),
-	`arrival` VARCHAR(100) DEFAULT NULL REFERENCES stops(town),
+	`departure` VARCHAR(100) REFERENCES towns(`name`),
+	`arrival` VARCHAR(100) REFERENCES towns(`name`),
 	`seats` int NOT NULL,
 	`registered` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`start` datetime DEFAULT NULL,
@@ -44,7 +51,7 @@ CREATE TABLE passenger (
 	`travelling`boolean DEFAULT false
 );
 
-INSERT INTO stops (`town`, `code`) VALUES
+INSERT INTO towns (`name`, `code`) VALUES
 ('Amiens', 80000),
 ('Abbeville', 80100),
 ('Albert', 80300),
@@ -63,7 +70,6 @@ INSERT INTO stops (`town`, `code`) VALUES
 ('Bapaume', 62450),
 ('Lassigny', 60310),
 ('Friville-Escarbotin', 80130),
-('Longueau', 80330),
 ('Acheux-en-Amiénois', 80560),
 ('Saleux', 80480),
 ('Boves', 80440),
@@ -74,4 +80,27 @@ INSERT INTO stops (`town`, `code`) VALUES
 ('Saint-Riquier', 80135),
 ('Querrieu', 80115),
 ('Cayeux-sur-Mer', 80410),
-('Naours', 80260);
+('Naours', 80260),
+('Longueau', 80330),
+('Dury', 80480),
+('Rivery', 80136),
+('Camon', 80450),
+('Cagny', 80330),
+('Flesselles', 80260),
+('Bouzincourt', 80300),
+('Pont-de-Metz', 80480),
+('Daours', 80800),
+('Glisy', 80440),
+('Saveuse', 80470),
+('Bacouel-sur-Selle', 80480),
+('Poulainville', 80320),
+('Saint-Fuscien', 80680),
+('Vers-sur-Selles', 80480),
+('Cachy', 80800),
+('Dreuil-lès-Amiens', 80470);
+
+
+INSERT INTO stops (`id`, `departure`, `arrival`, `mac`) VALUES
+(496, 'Rivery', 'Amiens', 'FB:86:61:5A:84:6B'),
+(497, 'Camon', 'Amiens', 'D7:07:FC:B6:E4:E7'),
+(498, 'Dury', 'Amiens', 'CA:0B:99:7B:2B:F7');
